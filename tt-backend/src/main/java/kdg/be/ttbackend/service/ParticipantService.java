6a4bffa7ef6d2ab4e3201d3b446308b276cef6cf
Kobe Ponet <kobe.ponet@student.kdg.be>
2024-11-22 15:12:18 +0100

    fixed add participant and created remove participant

--- tt-backend/src/main/java/kdg/be/ttbackend/service/ParticipantService.java
+++ tt-backend/src/main/java/kdg/be/ttbackend/service/ParticipantService.java
@@ -20,10 +20,38 @@ public class ParticipantService {
     }
 
     public Participant addParticipant(Participant participant, Long groupId) {
-        Optional<Group> group = groupRepository.findById(groupId);
-        group.ifPresent(participant::setGroup);
-        return participantRepository.save(participant);
+        Optional<Group> groupOptional = groupRepository.findById(groupId);
+        if (groupOptional.isPresent()) {
+            Group group = groupOptional.get();
+            if (group.getParticipants().size() < group.getMaxParticipants() || group.getMaxParticipants() == null) {
+                participant.setGroup(group);
+                return participantRepository.save(participant);
+            } else {
+                throw new IllegalStateException("The group has reached the maximum number of participants.");
+            }
+        } else {
+            throw new IllegalArgumentException("Group not found.");
+        }
     }
 
-    //TODO: removeParticipant
+    public void removeParticipant(Long participantId, Long groupId) {
+        Optional<Group> groupOptional = groupRepository.findById(groupId);
+        if (groupOptional.isPresent()) {
+            Group group = groupOptional.get();
+            Optional<Participant> participantOptional = participantRepository.findById(participantId);
+            if (participantOptional.isPresent()) {
+                Participant participant = participantOptional.get();
+                if (group.getParticipants().contains(participant)) {
+                    group.getParticipants().remove(participant);
+                    participantRepository.delete(participant);
+                } else {
+                    throw new IllegalArgumentException("Participant not found in the group.");
+                }
+            } else {
+                throw new IllegalArgumentException("Participant not found.");
+            }
+        } else {
+            throw new IllegalArgumentException("Group not found.");
+        }
+    }
 }
